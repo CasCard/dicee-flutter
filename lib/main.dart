d4a9287876c7de7e809bbb6338db7ae63240cd6c
Abel C Dixon <abelcheruvathoor@gmail.com>
2019-12-02 09:17:28 +0530

    Updated states of Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -23,6 +24,12 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 3;
   int rightDiceNumber=4;
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -30,21 +37,13 @@ class _DicePageState extends State<DicePage> {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = 5;
-                });
-              },
+              onPressed: changeDiceFace,
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rightDiceNumber = 6;
-                });
-              },
+              onPressed: changeDiceFace,
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
